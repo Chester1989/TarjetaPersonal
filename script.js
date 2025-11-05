90be6cf5803bc50bfd6a8f2f35d27e7c3f3765cd
alejandro gonzalez flores <agf_001@outlook.com>
2025-11-05 13:12:46 -0800

    Cambiando script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const boton= document.getElementById("botonMensaje");
+const mensaje= document.getElementById("mensaje");
 
 //Añadimos un evento que se activa al hacer click en el botón
 boton.addEventListener("click",()=>{
-    alert("¡Hola! Gracias por visitar mi Tarjeta Personal.");
+    mensaje.textContent="¡Gracias por visitar mi Tarjeta Personal!";
 });
