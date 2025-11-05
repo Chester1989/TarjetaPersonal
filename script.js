6827cb6b13f66aae6ec677d56aec2e8e6862e362
alejandro gonzalez flores <agf_001@outlook.com>
2025-11-05 12:59:10 -0800

    Creando script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton= document.getElementById("botonMensaje");
+
+//Añadimos un evento que se activa al hacer click en el botón
+boton.addEventListener("click",()=>{
+    alert("¡Hola! Gracias por visitar mi Tarjeta Personal.");
+});
